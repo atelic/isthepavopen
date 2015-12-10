0ee6f68a5982ba7598cfb2c2d9ad0f8d1f63c09a
Eric Barbour <emb4gu@virginia.edu>
2015-12-09 19:56:27 -0500

    Add hours page and dev branch

--- index.py
+++ index.py
@@ -1,37 +1,40 @@
 from datetime import datetime, time
 from flask import Flask, render_template
+
+from settings import debug
 app = Flask(__name__)
 
-# As of 09-12-2015, the hours for Pavilion XI (The Pav) are:
-# Monday - Thursday    Friday               Saturday    Sunday
-# 10:30 am - 8:00 pm   10:30 am - 6:00 pm   Closed      Closed
-#
-# Per: http://www.virginia.edu/newcomb/building-hours/
-# There is currently no way to programmatically find these
+
 
 OPEN = time(10, 30)
 M_TH_CLOSE = time(20, 00)
 F_CLOSE = time(18, 30)
+ME_CLOSE = time(16, 00)
 
 
 @app.route('/')
 def index():
     time_now = datetime.now().time()
     day = datetime.today().weekday()
-    close = False
+    closing = False
     if day in range(0, 4):
-        close = M_TH_CLOSE
+        closing = M_TH_CLOSE
     if day == 4:
-        close = F_CLOSE
+        closing = F_CLOSE
 
     open = is_open(time_now, day)
     meal = is_meal(time_now, day)
 
-    return render_template('index.html', open=open, meal=meal, close=close)
+    return render_template('index.html', open=open, meal=meal, close=closing)
 
 
 def is_open(time_now, day):
-
+    """ As of 09-12-2015, the hours for Pavilion XI (The Pav) are:
+    Monday - Thursday  Friday            Saturday    Sunday
+    10:30am - 8:00pm   10:30am - 6:00pm  Closed      Closed
+    Per: http://www.virginia.edu/newcomb/building-hours/
+    There is currently no way to programmatically find these
+    """
     hours = {
         'open': OPEN,
         'close': time(00, 00)
@@ -50,16 +53,22 @@ def is_open(time_now, day):
 
 
 def is_meal(time_now, day):
+    """ Monday - Thursday 4:00pm - 8:00pm"""
     hours = {
-        'open': time(16, 00),
+        'open': ME_CLOSE,
         'close': M_TH_CLOSE
     }
     meal = False
     weekday = day not in range(4, 7)
+
     if time_now >= hours['open'] and time_now <= hours['close'] and weekday:
         meal = True
 
     return meal
 
+@app.route('/hours')
+def hours():
+    return render_template('hours.html')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=debug)
